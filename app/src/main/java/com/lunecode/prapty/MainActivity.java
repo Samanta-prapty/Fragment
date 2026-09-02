48394c40aee68ed1144cdbba6c5f64d842085b67
Samanta Hossain Prapty <hossain241-15-185@diu.edu.bd>
2026-09-02 13:29:05 +0600

    commit

--- app/src/main/java/com/lunecode/prapty/MainActivity.java
+++ app/src/main/java/com/lunecode/prapty/MainActivity.java
@@ -1,14 +1,21 @@
 package com.lunecode.prapty;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout navHome, navExplore, navProfile, navLibrary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        navHome = findViewById(R.id.navHome);
+        navProfile = findViewById(R.id.navProfile);
+        navExplore = findViewById(R.id.navExplore);
+        navLibrary = findViewById(R.id.navLibrary);
+
+        loadFragment(new HomeFragment());
+        initialization();
+        navHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HomeFragment());
+
+            }
+        });
+
+        navExplore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ExplorFragment());
+
+            }
+        });
+        navProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ProfileFragment());
+
+            }
+        });
+
+        navLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new LibraryFragment());
+
+            }
+        });
+    }
+    private void initialization()
+    {
+
+    }
+    private void loadFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout, fragment);
+        fragmentTransaction.commitAllowingStateLoss();
     }
 }
